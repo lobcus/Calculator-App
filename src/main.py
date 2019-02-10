32aab3543d1504b72a797f5c7dff1f000abd6368
lobcus <lobcus@gmail.com>
2019-02-10 14:56:34 -0500

    Fixed title and footer

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+# app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 
 
 @app.route("/")
